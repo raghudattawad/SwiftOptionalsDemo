5383956044ebd69c26d48b1f0ea8358f1d07e5a8
Raghavendra Dattawad <raghavendra.d@wickedride.com>
2018-07-25 02:57:41 -0700

    Initial Commit

---
+++ SwifOptionalDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwifOptionalDemo
+//
+//  Created by Raghavendra Dattawad on 7/25/18.
+//  Copyright © 2018 Raghavendra Dattawad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
